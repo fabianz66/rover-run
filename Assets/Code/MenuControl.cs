998fe365a4d85a33644603875d588fb03ecb81c2
”Fabian <fabianz66@gmail.com>
2020-05-17 21:56:43 -0400

    Added mute to menu

--- Assets/Code/MenuControl.cs
+++ Assets/Code/MenuControl.cs
@@ -1,12 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuControl : MonoBehaviour
 {
+    [SerializeField]
+    public Button MuteUnmuteButton;
+
+    [SerializeField]
+    public Sprite MutedSprite;
+
+    [SerializeField]
+    public Sprite UnmutedSprite;
+
     private void Start()
     {
+        //Initial UI
+        if (AudioListener.volume == 0)
+        {
+            MuteUnmuteButton.image.sprite = MutedSprite;
+        }
+        else
+        {
+            MuteUnmuteButton.image.sprite = UnmutedSprite;
+        }
         Time.timeScale = 1.0f;
     }
 
@@ -23,5 +42,19 @@ public class MenuControl : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene("GameScene");        
+    }
+
+    public void MuteUnmute()
+    {
+        if (AudioListener.volume == 0)
+        {
+            AudioListener.volume = 1;
+            MuteUnmuteButton.image.sprite = UnmutedSprite;
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            MuteUnmuteButton.image.sprite = MutedSprite;
+        }
     }
 }
