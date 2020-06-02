b97926583268ed268d5c3377f0fdfc7434559196
”Fabian <fabianz66@gmail.com>
2020-06-01 23:19:05 -0400

    Trying to destroy

---
+++ Assets/Code/SelfDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroy : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("POSITION: " + this.transform.position.y);
+        if (this.transform.position.y < -4) {
+            Destroy(this);
+        }
+    }
+}
