cab1798d8bb4d0eda5cfdeb32ea9facb291a9500
”Fabian <fabianz66@gmail.com>
2020-06-02 17:03:45 -0400

    Destroy objects when the are off the screen

--- Assets/Code/SelfDestroy.cs
+++ Assets/Code/SelfDestroy.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class SelfDestroy : MonoBehaviour
 {
-    // Update is called once per frame
+    private Camera MainCamera;
+    private float CameraHalfWidth;
+    private float ObjectWidth;
+
+    private void Start()
+    {
+        ObjectWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+        MainCamera = Camera.main;
+        float halfHeight = MainCamera.orthographicSize;
+        CameraHalfWidth = MainCamera.aspect * halfHeight;
+    }
+
     void Update()
     {
-        //Debug.Log("POSITION: " + this.transform.position.y);
-        if (this.transform.position.y < -4) {
-            Destroy(this);
+        float objectX = (this.transform.position.x + ObjectWidth);
+        // MainCamera.transform.position.x gives the center of the camera
+        float cameraStartX = MainCamera.transform.position.x - CameraHalfWidth;
+        if (objectX < cameraStartX) {
+            Destroy(this.gameObject);
         }
     }
 }
